d86e9c4ad151fd4a5dced2500d3cde8d561f50a8
mcclayac <anthony.mcclay@me.com>
2020-08-05 20:44:38 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print('Dart Quick Start Chapter 4');
+  print('4. Inheritance and Mixins in Dart');
+  //https://learning.oreilly.com/library/view/quick-start-guide/9781484255629/A490744_1_En_4_Chapter.html
+
+}
